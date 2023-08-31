fdd7aefbd3810d16d09108e045849e485a4595c2
tina-bardot <142334011+tina-bardot@users.noreply.github.com>
2023-08-31 11:00:02 -0400

    Update rel risk indicators
    Added risk for PFAS and LCR violations; note that for PFAS there are a lot of different ways to measure (concentration, count of violations, etc) so I'm making a separate file that has supplemental calculations (so that this one only contains one measure per indicator)

--- code/rel_risk_indicators.R
+++ code/rel_risk_indicators.R
@@ -37,31 +37,12 @@ getwd()
 
 ################################################################################
 ## Load Data 
+## Health violations
 ################################################################################
 
 # Dataset on health-based violations (since 2015, national) 
 
-HB_vio <- read.csv("Data/health_violations2015.csv")
-
-pwsid_cbg <- read.csv("Data/sb_dems_area_v3.csv")
-
-# Rename and convert CBG ID to string with leading 0s if necessary  
-pwsid_cbg <- pwsid_cbg %>%
-  rename(PWSID = pwsid)
-
-# Left join the full PWSID dataset with the health violations data
-
-HB_vio <- left_join(pwsid_cbg, HB_vio, by = "PWSID", relationship = "many-to-many")  %>%
-  mutate(ID=str_pad(ID, 12, pad="0"))
-
-rm(pwsid_cbg) #remove files if unnecessary
-
-# For tracts with PWSIDs, and null values for the number of violations, replace nulls 
-# with zeros
-
-HB_vio$total_violations[is.na(HB_vio$total_violations)] <- 0
-HB_vio$diff_days[is.na(HB_vio$diff_days)] <- 0
-
+HB_vio <- read_rds("Data/cbg_HB_vio_combined.rds")
 
 ################################################################################
 ## Data manipulation : Race indicator
@@ -125,4 +106,152 @@ rel_risk_inc <- POV_risk / HIINC_risk
 rel_risk_inc
 
 # 0.5633946
+
+# Clear memory
+rm(cbg_HB_vio)
+
+################################################################################
+## Load Data 
+## LCR violations 
+################################################################################
+
+LCR_vio <- read_rds("Data/cbg_LCR_vio_comb.rds")
+
+################################################################################
+## Data manipulation : Race indicator
+################################################################################
+
+## Relative risk of POC and non-hispanic whites
+
+LCR_vio <- LCR_vio %>%
+  mutate(POC_vio = sum(MINORPCT*population_served_count*total_violations))
+
+POC_risk <- (LCR_vio$POC_vio/sum(LCR_vio$population_served_count))
+
+POC_risk
+
+# 1.130441  
+
+LCR_vio <- LCR_vio %>%
+  mutate(WHITEPCT = (1-MINORPCT)) %>%
+  mutate(WHI_vio = sum(WHITEPCT*population_served_count*total_violations))
+
+WHI_risk <- (LCR_vio$WHI_vio/sum(LCR_vio$population_served_count))
+
+WHI_risk
+
+# 0.7051423  
+
+rel_risk_race <- POC_risk / WHI_risk
+
+rel_risk_race
+
+# 1.603139 
+
+
+################################################################################
+## Data manipulation: Income indicator
+################################################################################
+
+## Relative risk of individuals above or below 2X the poverty limit
+
+LCR_vio <- LCR_vio %>%
+  mutate(POV_vio = sum(LOWINCPCT*population_served_count*total_violations))
+
+POV_risk <- (LCR_vio$POV_vio/sum(LCR_vio$population_served_count))
+
+POV_risk
+
+# 0.6262008  
+
+LCR_vio <- LCR_vio %>%
+  mutate(HIINC = (1-LOWINCPCT)) %>%
+  mutate(HIINC_vio = sum(HIINC*population_served_count*total_violations))
+
+HIINC_risk <- (LCR_vio$HIINC_vio/sum(LCR_vio$population_served_count))
+
+HIINC_risk
+
+# 1.209383 
+
+rel_risk_inc <- POV_risk / HIINC_risk
+
+rel_risk_inc
+
+# 0.5177855 
+
+# Clear memory
+rm(cbg_LCR_vio)
+
+################################################################################
+## Load Data 
+## PFAS violations
+################################################################################
+
+pfas_vio <- read_rds("Data/cbg_pfas_vio_combined.rds")
+
+################################################################################
+## Data manipulation : Race indicator
+################################################################################
+
+pfas_vio <- pfas_vio %>%
+  mutate(POC_vio = sum(MINORPCT*population_served_count*concentration_sum))
+
+POC_risk <- (pfas_vio$POC_vio/sum(pfas_vio$population_served_count))
+
+POC_risk
+
+# 6.276714  
+
+pfas_vio <- pfas_vio %>%
+  mutate(WHITEPCT = (1-MINORPCT)) %>%
+  mutate(WHI_vio = sum(WHITEPCT*population_served_count*concentration_sum))
+
+WHI_risk <- (pfas_vio$WHI_vio/sum(pfas_vio$population_served_count))
+
+WHI_risk
+
+# 4.738303 
+
+rel_risk_race <- POC_risk / WHI_risk
+
+rel_risk_race
+
+# 1.324676
+
+
+################################################################################
+## Data manipulation: Income indicator
+################################################################################
+
+## Relative risk of individuals above or below 2X the poverty limit
+
+pfas_vio <- pfas_vio %>%
+  mutate(POV_vio = sum(LOWINCPCT*population_served_count*concentration_sum))
+
+POV_risk <- (pfas_vio$POV_vio/sum(pfas_vio$population_served_count))
+
+POV_risk
+
+# 3.355327 
+
+pfas_vio <- pfas_vio %>%
+  mutate(HIINC = (1-LOWINCPCT)) %>%
+  mutate(HIINC_vio = sum(HIINC*population_served_count*concentration_sum))
+
+HIINC_risk <- (pfas_vio$HIINC_vio/sum(pfas_vio$population_served_count))
+
+HIINC_risk
+
+# 7.65969
+
+rel_risk_inc <- POV_risk / HIINC_risk
+
+rel_risk_inc
+
+# 0.4380499
+
+# Clear memory
+rm(pfas_vio)
+
 
