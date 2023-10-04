e1d14a6dcc4ec13d8ea44d6ddc54a66cd7f2fddf
tina-bardot <142334011+tina-bardot@users.noreply.github.com>
2023-10-04 16:09:17 -0400

    Create 04_country_bivariate_maps.R
    doesn't include LCR maps (haven't produced them yet)

---
+++ code/04_country_bivariate_maps.R
@@ -0,0 +1,518 @@
+################################################################################
+# Bivariate maps for drinking water indicators : country maps
+# National Center for Environmental Economics
+# Last edited: 9/29/30
+################################################################################
+
+# Drawing from code developed by Wes Austin for a biscale function with action levels 
+
+# Load packages
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  readr,
+  tidyverse,
+  sf,
+  cowplot,
+  patchwork,
+  dplyr, # data wrangling
+  mapview,
+  biscale,
+  ggplot2,
+  tidycensus, #Census data
+  readr,
+  raster, # raster data operations
+  exactextractr, # fast raster data extraction for polygons
+  data.table, # data wrangling
+  dplyr, # data wrangling
+  lubridate, # Date object handling
+  tmap, # for map creation
+  cowplot, # for bivariate mapping
+  stringr, # string manipulation
+  mapview,
+  pals,
+  colorblindcheck
+)
+
+################################################################################
+##Set directories
+################################################################################
+
+my_path <- "C:/Users/tbardot/OneDrive - Environmental Protection Agency (EPA)/Documents/EJ Water systems"
+
+getwd()
+setwd(paste0(my_path))
+getwd()
+
+plot_path <- "Plots/country/"
+
+# Source code for bi_scale manipulations
+
+source("Code/biscale_fn_action_level.R")
+
+## Color palette 
+
+custom_pal3_4 <- c(
+  "1-1" = "#d3d3d3", # low x, low y
+  "2-1" = "#9c9ecd",
+  "3-1" = "#646ac7", # high x, low y
+  "1-2" = "#c3c594",
+  "2-2" = "#909390", # medium x, medium y
+  "3-2" = "#5c638c",
+  "1-3" = "#b1b552", # low x, high y
+  "2-3" = "#83884f",
+  "3-3" = "#545b4d" # high x, high y
+)
+bi_pal(pal = custom_pal3_4, dim = 3)
+
+#Check for types of colorblindness
+
+palette_check(custom_pal3_3, plot = TRUE) #okay
+
+################################################################################
+## Load tract data for all US
+################################################################################
+
+# Following code to retreive ALL CBG-level data for the US
+
+US_cbg <- tigris::block_groups(state = NULL, county = NULL, cb = TRUE) 
+
+st_crs(US_cbg)
+
+US_cbg_50 <- US_cbg %>%
+  rename(ID = GEOID) %>%
+  tigris::shift_geometry() %>%
+  mutate(STATEFP = as.numeric(STATEFP)) %>%
+  dplyr::filter(STATEFP <= 56 & STATEFP != 43 ) %>%  #remove Puerto Rico and outer territories
+  st_transform(crs = 5070) # this CRS gives the neatest projections
+
+################################################################################
+## Health violations
+################################################################################
+
+cbg_HB_vio <- read_rds("Data/combined/cbg_HB_vio_combined.rds") 
+
+# For cbg with more than one PWSID, find average number of violations at the cbg level
+# then collapse to include only 1 observation per census cbg level (12-digit "ID") 
+
+cbg_HB_vio <- cbg_HB_vio %>%
+  group_by(ID) %>%
+  mutate(avg_vio_cbg = sum(total_violations*ACSTOTPOP)/sum(ACSTOTPOP)) %>%
+  mutate(avg_vio_length = mean(diff_days)) %>%
+  distinct(ID, .keep_all = TRUE) 
+
+HB_vio_all <- left_join(US_cbg_50, cbg_HB_vio) 
+
+st_as_sf(HB_vio_all)
+
+################################################################################
+## Create bivariate map
+################################################################################
+
+# 1. define the action level. Currently, the function takes two numbers for the action_vector, can be modified
+
+summary(HB_vio_all$avg_vio_cbg)
+
+#3rd qt = 2
+
+#3rd quartile = 2, so we will use that as a limit for "frequent violators"
+
+action_vector <- c(0.9,2) #Modify for indicator-specific
+
+#Establish biscale comparison: HB violations and % POC
+
+data_biscale_al <- bi_class_al(HB_vio_all,
+                               x = avg_vio_cbg, y = MINORPCT, #% POC
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Health based violations and % POC
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = "BlueOr", dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = "BlueOr",
+                    dim = 3,
+                    xlab = "Violations",
+                    ylab = "% POC",
+                    size = 20)
+
+# Plot and legend
+
+hb_minor <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+
+png(file = paste0(plot_path,"hb_minor_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(hb_minor)
+
+dev.off()
+
+
+#Establish biscale comparison: HB violations and % POC
+
+data_biscale_al <- bi_class_al(HB_vio_all,
+                               x = avg_vio_cbg, y = LOWINCPCT, #%Low income
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Biscale violations: HB violations and low income
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = custom_pal3_4, dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = custom_pal3_4,
+                    dim = 3,
+                    xlab = "Violations",
+                    ylab = "% Low Inc",
+                    size = 20)
+
+# Plot and legend
+
+hb_lowinc <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"hb_lowinc_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(hb_lowinc)
+
+dev.off()
+
+
+rm(HB_vio_all, cbg_HB_vio, hb_lowinc, hb_minor)
+
+
+################################################################################
+## PFAS violations
+################################################################################
+
+cbg_pfas_vio <- read_rds("Data/combined/cbg_pfas_vio_combined.rds") #load pfas data
+
+# CBG
+
+cbg_pfas_vio <- cbg_pfas_vio %>%
+  group_by(ID) %>%
+  mutate(pfas_count_cbg = sum(pfas_count*ACSTOTPOP)/sum(ACSTOTPOP)) %>% #avg pfas count per cbg
+  mutate(avg_conc_cbg = sum(concentration_sum*ACSTOTPOP)/sum(ACSTOTPOP)) %>% #avg pfas concentration per cbg
+  distinct(ID, .keep_all = TRUE) 
+
+pfas_vio_all <- left_join(US_cbg_50, cbg_pfas_vio) 
+
+st_as_sf(pfas_vio_all) #set as spatial object
+
+################################################################################
+## Create bivariate map
+################################################################################
+
+# 1. define the action level. Currently, the function takes two numbers for the action_vector, can be modified
+
+summary(pfas_vio_all$avg_conc_cbg)
+
+summary(pfas_vio_all$pfas_count_cbg)
+
+#3rd quartile = 1, so we will use that as a limit for "frequent violators"
+
+action_vector <- c(0.01,1) #Modify for indicator-specific
+
+#Establish biscale comparison: pfas violations and % POC
+
+data_biscale_al <- bi_class_al(pfas_vio_all,
+                               x = pfas_count_cbg, y = MINORPCT, #% POC
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+
+# Map 1: Health based violations and % POC
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = "BlueOr", dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = "BlueOr",
+                    dim = 3,
+                    xlab = "# PFAS Detected",
+                    ylab = "% POC",
+                    size = 20)
+
+# Plot and legend
+
+pfas_minor <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"pfas_minor_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(pfas_minor)
+
+dev.off()
+
+
+#Establish biscale comparison: pfas violations and % low income
+
+data_biscale_al <- bi_class_al(pfas_vio_all,
+                               x = pfas_count_cbg, y = LOWINCPCT, #%Low income
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Biscale violations: pfas violations and low income
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = custom_pal3_4, dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = custom_pal3_4,
+                    dim = 3,
+                    xlab = "# PFAS Detected",
+                    ylab = "% Low Inc",
+                    size = 20)
+
+# Plot and legend
+
+pfas_lowinc <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"pfas_lowinc_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(pfas_lowinc)
+
+dev.off()
+
+
+rm(pfas_vio_all, cbg_pfas_vio, pfas_lowinc, pfas_minor)
+
+################################################################################
+## DBP violations
+################################################################################
+
+cbg_dbp_vio <- read_rds("Data/combined/cbg_dbp_vio_combined.rds")
+
+# CBG
+
+cbg_dbp_vio <- cbg_dbp_vio %>%
+  group_by(ID) %>%
+  mutate(avg_dbp_cbg = sum(combined_dbp*ACSTOTPOP)/sum(ACSTOTPOP)) %>%
+  distinct(ID, .keep_all = TRUE) 
+
+dbp_vio_all <- left_join(US_cbg_50, cbg_dbp_vio) 
+
+st_as_sf(dbp_vio_all) #set as spatial object
+
+################################################################################
+## Create bivariate map
+################################################################################
+
+# 1. define the action level. Currently, the function takes two numbers for the action_vector, can be modified
+
+summary(dbp_vio_all$avg_dbp_cbg) #distribution
+
+#3rd quartile = 64, so we will use that as a limit for "frequent violators"
+
+action_vector <- c(0.9,60) #Modify for indicator-specific
+
+#Establish biscale comparison: dbp violations and % POC
+
+data_biscale_al <- bi_class_al(dbp_vio_all,
+                               x = avg_dbp_cbg, y = MINORPCT, #% POC
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Health based violations and % POC
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = "BlueOr", dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = "BlueOr",
+                    dim = 3,
+                    xlab = "Higher DBP Concentration",
+                    ylab = "Higher % POC",
+                    size = 20)
+
+# Plot and legend
+
+dbp_minor <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"dbp_minor_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(dbp_minor)
+
+dev.off()
+
+
+#Establish biscale comparison: dbp violations and % low income
+
+data_biscale_al <- bi_class_al(dbp_vio_all,
+                               x = avg_dbp_cbg, y = LOWINCPCT, #%Low income
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Biscale violations: dbp violations and low income
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = custom_pal3_4, dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = custom_pal3_4,
+                    dim = 3,
+                    xlab = "Higher DBP Concentration",
+                    ylab = "% Low Income",
+                    size = 20)
+
+# Plot and legend
+
+dbp_lowinc <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"dbp_lowinc_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(dbp_lowinc)
+
+dev.off()
+
+
+rm(dbp_vio_all, cbg_dbp_vio, dbp_lowinc, dbp_minor)
+
+################################################################################
+## TCR violations
+################################################################################
+
+cbg_tcr_vio <-read_rds("Data/combined/cbg_tcr_vio_combined.rds")
+
+# CBG
+
+cbg_tcr_vio <- cbg_tcr_vio %>%
+  group_by(ID) %>%
+  mutate(tcr_det_cbg = sum(detection_share*ACSTOTPOP)/sum(ACSTOTPOP)) %>%
+  distinct(ID, .keep_all = TRUE)
+
+tcr_vio_all <- left_join(US_cbg_50, cbg_tcr_vio) 
+
+st_as_sf(tcr_vio_all) #set as spatial object
+
+
+################################################################################
+## Create bivariate map
+################################################################################
+
+# 1. define the action level. Currently, the function takes two numbers for the action_vector, can be modified
+
+summary(tcr_vio_all$tcr_det_cbg) #distribution
+
+#3rd quartile = 0.01, but we can use .25 as a limit for "high concentrations"
+
+action_vector <- c(0.01,.25) #Modify for indicator-specific
+
+#Establish biscale comparison: tcr violations and % POC
+
+data_biscale_al <- bi_class_al(tcr_vio_all,
+                               x = tcr_det_cbg, y = MINORPCT, #% POC
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Health based violations and % POC
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = "BlueOr", dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = "BlueOr",
+                    dim = 3,
+                    xlab = "Higher TCR Concentration",
+                    ylab = "Higher % POC",
+                    size = 20)
+
+# Plot and legend
+
+tcr_minor <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"tcr_minor_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(tcr_minor)
+
+dev.off()
+
+
+#Establish biscale comparison: tcr violations and % low income
+
+data_biscale_al <- bi_class_al(tcr_vio_all,
+                               x = tcr_det_cbg, y = LOWINCPCT, #%Low income
+                               style = "quantile", dim = 3,
+                               action_level = T, action_vector = action_vector) %>%
+  filter(!str_detect(bi_class, 'NA'))
+
+# Map 1: Biscale violations: tcr violations and low income
+
+map <- ggplot(data_biscale_al) +
+  geom_sf(data = data_biscale_al, mapping = aes(fill = bi_class), color = NA , size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = custom_pal3_4, dim = 3) +
+  bi_theme() +
+  theme(plot.background = element_blank())
+
+legend <- bi_legend(pal = custom_pal3_4,
+                    dim = 3,
+                    xlab = "Higher TCR Concentration",
+                    ylab = "% Low Income",
+                    size = 20)
+
+# Plot and legend
+
+tcr_lowinc <- cowplot::ggdraw() +
+  draw_plot(map, x = 0, y = 0, width = 1, height = 1) +
+  draw_plot(legend, x = 0.1, y = 0.05, width = 0.2, height = 0.2)
+
+png(file = paste0(plot_path,"tcr_lowinc_biv_US.png"), 
+    width = 1915, height = 1077, units = "px", pointsize = 12,
+    bg = "transparent")
+
+print(tcr_lowinc)
+
+dev.off()
+
+
+rm(tcr_vio_all, cbg_tcr_vio, tcr_lowinc, tcr_minor)
