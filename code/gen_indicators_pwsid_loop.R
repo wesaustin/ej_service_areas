a6a237291e8d4384d41e0fa4588d73abad230740
tina-bardot <142334011+tina-bardot@users.noreply.github.com>
2024-04-19 11:48:41 -0400

    Add loop files for rel risk and boundaries
    "indicator boundaries" are the CBG level boundaries for each indicator, "pwsid loop" is for the pwsid-level data (no CBG ids, used for the rel risk); rel risk loops include all boundaries, so moving all boundary-specific rel risk to the archive folder

---
+++ code/gen_indicators_pwsid_loop.R
@@ -0,0 +1,285 @@
+################################################################################
+## Loop functions to generate indicator datasets by indicator
+## NCEE EPA 
+## Last edited: 2/16/2024
+################################################################################
+
+# Load packages
+
+# Data cleaning packages
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  readr,
+  tidyverse,
+  data.table, # data wrangling
+  dplyr, # data wrangling
+  lubridate, # Date object handling
+  modelsummary, # regression table generation
+  stringr, # string manipulation
+  MASS , #For regressions and modeling
+  dplyr, #data manipulation
+  readxl, #read excel files
+  janitor #cleaning names
+)
+
+################################################################################
+##Set directories
+################################################################################
+
+#Tina
+my_path <- "C:/Users/tbardot/OneDrive - Environmental Protection Agency (EPA)/Documents/EJ Water systems"
+
+#WA
+#my_path <- "C:/Users/gaustin/OneDrive - Environmental Protection Agency (EPA)/NCEE - Water System Service Boundaries"
+
+getwd()
+setwd(paste0(my_path))
+getwd()
+
+################################################################################
+## Create indicator datasets for different boundaries by indicator
+################################################################################
+
+# Call different boundary types
+
+boundary <- c("epic", "usgs", "zc", "county", "hm")
+
+# Columns to convert to numeric
+
+cols_num <- c("pop_served", "minorpct", "lowinc", "frac_black", "frac_hisp", "frac_amerind", "frac_asian", "frac_pacisl", "frac_hisp")
+
+# Load base indicator violations data
+
+# i = "usgs"
+
+HB_vio <- read.csv("Data/health_violations_2015.csv") 
+
+# Create loop function to call each boundary and combine with indicator
+
+for (i in boundary) {
+  
+  # Load the demographic datasets by boundary
+  pwsid_dem <- read.csv(paste0("Data/demographics/", i, "_dems_final.csv")) %>%
+    clean_names() %>%
+    dplyr::select(-ends_with("state")) %>%
+    rename_with(
+      ~ case_when(
+        . == "lowincpct" ~ "lowinc",
+        . == "population_served_count" | . == "population" | . == "tpopsrv" ~ "pop_served",
+        . == "id" ~ "ID",
+        TRUE ~ . 
+      )
+    ) %>%
+    filter(!is.na(minorpct)) %>%
+    mutate_at(cols_num, as.numeric) 
+  
+  
+  # Left join the boundary to violations data to get cbg-level data
+  cbg_vio <- left_join(pwsid_dem, HB_vio, by = "pwsid", relationship = "many-to-many") 
+  
+  #Perform indicator-specific operations: fill in zeroes for HB violations 
+  cbg_vio <- cbg_vio %>%
+    mutate(total_violations = coalesce(total_violations, 0),
+           dist_enf = coalesce(dist_enf, 0),
+           dist_vio_type = coalesce(dist_vio_type, 0),
+           diff_days_max = coalesce(diff_days_max, 0)) 
+  
+  # Find the Duplicated Columns
+  duplicated_columns <- duplicated(t(cbg_vio))
+  
+  # Show the Names of the Duplicated Columns
+  colnames(cbg_vio[duplicated_columns])
+  
+  # Remove the Duplicated Columns
+  cbg_vio <- cbg_vio[!duplicated_columns]
+  
+  #Save the dataset
+  saveRDS(cbg_vio, file = paste0("Data/combined/pwsid/HB_vio_", i, ".rds"))
+  
+}
+
+rm(HB_vio)
+
+
+################################################################################
+## Lead violations
+################################################################################
+
+# Load lead violations
+
+lcr_vio <- readRDS("Data/lcr_violations.rds") %>%
+  clean_names
+
+for (i in boundary) {
+  
+  pwsid_dem <- read.csv(paste0("Data/demographics/", i, "_dems_final.csv")) %>%
+    clean_names() %>%
+    dplyr::select(-ends_with("state")) %>%
+    rename_with(
+      ~ case_when(
+        . == "lowincpct" ~ "lowinc",
+        . == "population_served_count" | . == "population" | . == "tpopsrv" ~ "pop_served",
+        . == "id" ~ "ID",
+        TRUE ~ . 
+      )
+    ) %>%
+    filter(!is.na(minorpct)) %>%
+    mutate_at(cols_num, as.numeric) 
+  
+  cbg_vio <- left_join(pwsid_dem, lcr_vio, by = "pwsid", relationship = "many-to-many") %>%
+    mutate(pb_vio_count = coalesce(pb_vio_count, 0),
+           cu_vio_count = coalesce(cu_vio_count, 0),
+           avg_cu_level = coalesce(avg_cu_level, 0),
+           avg_pb_level = coalesce(avg_pb_level, 0),
+           max_cu_level = coalesce(max_cu_level, 0),
+           max_pb_level = coalesce(max_pb_level, 0))
+  
+  
+  # Find the Duplicated Columns
+  duplicated_columns <- duplicated(t(cbg_vio))
+  
+  # Show the Names of the Duplicated Columns
+  colnames(cbg_vio[duplicated_columns])
+  
+  # Remove the Duplicated Columns
+  cbg_vio <- cbg_vio[!duplicated_columns]
+  
+  #Save the dataset
+  saveRDS(cbg_vio, file = paste0("Data/combined/pwsid/lcr_vio_", i, ".rds"))
+  
+}
+
+rm(lcr_vio)
+
+################################################################################
+## PFAS concentrations
+################################################################################
+
+# Load pfas violations data 
+
+pfas_vio <- read.csv("Data/indicators_pfas.csv") %>%
+  clean_names()
+
+for (i in boundary) {
+  
+  pwsid_dem <- read.csv(paste0("Data/demographics/", i, "_dems_final.csv")) %>%
+    clean_names() %>%
+    dplyr::select(-ends_with("state")) %>%
+    rename_with(
+      ~ case_when(
+        . == "lowincpct" ~ "lowinc",
+        . == "population_served_count" | . == "population" | . == "tpopsrv" ~ "pop_served",
+        . == "id" ~ "ID",
+        TRUE ~ . 
+      )
+    ) %>%
+    filter(!is.na(minorpct)) %>%
+    mutate_at(cols_num, as.numeric) 
+  
+  cbg_vio <- left_join(pwsid_dem,pfas_vio, by = "pwsid", relationship = "many-to-many") 
+    
+  
+  
+  # Find the Duplicated Columns
+  duplicated_columns <- duplicated(t(cbg_vio))
+  
+  # Show the Names of the Duplicated Columns
+  colnames(cbg_vio[duplicated_columns])
+  
+  # Remove the Duplicated Columns
+  cbg_vio <- cbg_vio[!duplicated_columns]
+  
+  #Save the dataset
+  saveRDS(cbg_vio, file = paste0("Data/combined/pwsid/pfas_vio_", i, ".rds"))
+  
+}
+
+rm(pfas_vio)
+
+################################################################################
+## Cumulative DBP
+################################################################################
+
+# Load DBP concentrations data
+
+dbp_vio <- read.csv("Data/indicator_dbp_v2.csv") %>%
+  clean_names()
+
+for (i in boundary) {
+  
+  pwsid_dem <- read.csv(paste0("Data/demographics/", i, "_dems_final.csv")) %>%
+    clean_names() %>%
+    dplyr::select(-ends_with("state")) %>%
+    rename_with(
+      ~ case_when(
+        . == "lowincpct" ~ "lowinc",
+        . == "population_served_count" | . == "population" | . == "tpopsrv" ~ "pop_served",
+        . == "id" ~ "ID",
+        TRUE ~ . 
+      )
+    ) %>%
+    filter(!is.na(minorpct)) %>%
+    mutate_at(cols_num, as.numeric) 
+  
+  cbg_vio <- left_join(pwsid_dem, dbp_vio, by = "pwsid", relationship = "many-to-many")
+  
+  
+  # Find the Duplicated Columns
+  duplicated_columns <- duplicated(t(cbg_vio))
+  
+  # Show the Names of the Duplicated Columns
+  colnames(cbg_vio[duplicated_columns])
+  
+  # Remove the Duplicated Columns
+  cbg_vio <- cbg_vio[!duplicated_columns]
+  
+  #Save the dataset
+  saveRDS(cbg_vio, file = paste0("Data/combined/pwsid/dbp_vio_", i, ".rds"))
+  
+}
+
+rm(dbp_vio)
+
+################################################################################
+## Cumulative TCR
+################################################################################
+
+# Load tcr concentrations data
+
+tcr_vio <- read.csv("Data/indicator_tcr.csv") %>%
+  clean_names()
+
+for (i in boundary) {
+  
+  pwsid_dem <- read.csv(paste0("Data/demographics/", i, "_dems_final.csv")) %>%
+    clean_names() %>%
+    dplyr::select(-ends_with("state")) %>%
+    rename_with(
+      ~ case_when(
+        . == "lowincpct" ~ "lowinc",
+        . == "population_served_count" | . == "population" | . == "tpopsrv" ~ "pop_served",
+        . == "id" ~ "ID",
+        TRUE ~ . 
+      )
+    ) %>%
+    filter(!is.na(minorpct)) %>%
+    mutate_at(cols_num, as.numeric) 
+  
+  cbg_vio <- left_join(pwsid_dem, tcr_vio, by = "pwsid", relationship = "many-to-many") 
+  
+  # Find the Duplicated Columns
+  duplicated_columns <- duplicated(t(cbg_vio))
+  
+  # Show the Names of the Duplicated Columns
+  colnames(cbg_vio[duplicated_columns])
+  
+  # Remove the Duplicated Columns
+  cbg_vio <- cbg_vio[!duplicated_columns]
+  
+  #Save the dataset
+  saveRDS(cbg_vio, file = paste0("Data/combined/pwsid/tcr_vio_", i, ".rds"))
+  
+}
+
+rm(tcr_vio)
---
+++ code/gen_rel_risk_loop.R
@@ -0,0 +1,315 @@
+################################################################################
+# Demographic Indices for SDWA indicators : sensitivity tests
+# National Center for Environmental Economics
+# Last edited: 11/27/2023
+################################################################################
+
+# Load packages
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  readr,
+  tidyverse,
+  data.table, # data wrangling
+  dplyr, # data wrangling
+  lubridate, # Date object handling
+  modelsummary, # regression table generation
+  stringr, # string manipulation
+  magrittr,
+  tidycensus, #Census data
+  MASS , #For regressions and modeling
+  dplyr,
+  readxl,
+  janitor
+)
+
+################################################################################
+##Set directories
+################################################################################
+
+#Tina
+my_path <- "C:/Users/tbardot/OneDrive - Environmental Protection Agency (EPA)/Documents/EJ Water systems"
+
+#WA
+# my_path <- "C:/Users/gaustin/OneDrive - Environmental Protection Agency (EPA)/NCEE - Water System Service Boundaries"
+
+getwd()
+setwd(paste0(my_path))
+getwd()
+
+
+################################################################################
+## Creating data frames at the indicator level
+################################################################################
+
+boundary = c("county", "zc", "usgs", "epic", "hm")
+
+indicator = c('hb', 'lcr', 'pfas', 'dbp', 'tcr')
+
+################################################################################
+## First will be all the relative risks
+################################################################################
+
+## Create a loop to get health based violations indicators
+
+# Set up the function call 
+
+all_risk <- data.frame()
+
+pws_obs <- data.frame(matrix(ncol = 3, nrow = 0))
+
+colnames(pws_obs) <- c("obs", "indicator", "boundary")
+
+for (b in boundary) {
+  
+  hb_vio <- readRDS(paste0('data/combined/pwsid/hb_vio_',b ,'.rds'))
+  
+  lcr_vio <- readRDS(paste0('data/combined/pwsid/lcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pb_vio_count)
+  
+  pfas_vio <- readRDS(paste0('data/combined/pwsid/pfas_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pfas_count)
+  
+  dbp_vio <- readRDS(paste0('data/combined/pwsid/dbp_vio_',b ,'.rds')) %>%
+    mutate(total_violations = combined_dbp)
+  
+  tcr_vio <- readRDS(paste0('data/combined/pwsid/tcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = detection_share)
+  
+  data_list <- list(hb_vio, lcr_vio, pfas_vio, dbp_vio, tcr_vio)
+  
+  my_df <- data.frame(risk_type = c("race", "income"))
+  
+  for (j in 1:length(data_list)) {
+    
+    water_data <- data_list[[j]]
+    
+    i = nth(indicator, j)
+    
+    rel_risk <- water_data %>%
+      filter(pop_served != 0)%>% #remove systems with no pop served
+      mutate(whitepct = (1-minorpct)) %>%
+      mutate(minor_served = minorpct*pop_served) %>%
+      mutate(nhw_served = whitepct*pop_served) %>% #non hispanic white served 
+      mutate(minor_risk = weighted.mean(total_violations, minor_served, na.rm= TRUE)) %>% #black population weight
+      mutate(nhw_risk = weighted.mean(total_violations, nhw_served, na.rm= TRUE)) %>%   #non-hispanic white
+      mutate(highinc = (1-lowinc)) %>% #create high income category
+      mutate(highinc_served = highinc*pop_served) %>% #Find total population served in each category
+      mutate(lowinc_served = lowinc*pop_served) %>%
+      mutate(highinc_risk = weighted.mean(total_violations, highinc_served, na.rm= TRUE)) %>% #High income weight
+      mutate(lowinc_risk = weighted.mean(total_violations, lowinc_served, na.rm= TRUE)) #low income weight
+    
+    rel_risk_r = as.numeric(rel_risk[1, "minor_risk"]) / as.numeric(rel_risk[1, "nhw_risk"])
+    rel_risk_i = as.numeric(rel_risk[1, "lowinc_risk"]) / as.numeric(rel_risk[1, "highinc_risk"])
+    
+    risks <- data.frame(risk = c(rel_risk_r, rel_risk_i))
+    
+    my_df <- cbind(my_df, risks)
+    
+    colnames(my_df)[colnames(my_df) == "risk"] <- paste0(i, "_rel_risk")
+    
+    rm(risks)
+    
+    obs <- sum(!is.na(rel_risk$total_violations))
+    
+    pws_obs[nrow(pws_obs) + 1, ] = c(obs, i, b)
+    
+  }
+  
+  write.csv(my_df, file = paste0("Data/rel_risk/rel_risk_", b, ".csv"))
+  
+  my_df <- my_df %>% 
+    mutate(boundary = as.factor(b))
+  
+  all_risk <- bind_rows(all_risk, my_df)  
+  
+}
+
+write.csv(all_risk, file = "Data/rel_risk/rel_risk_all.csv")
+
+
+all_risk <- read.csv("Data/rel_risk/rel_risk_all.csv") 
+
+################################################################################
+## Create base to subset to tier 1 and 2 in EPIC and HQ states
+################################################################################
+
+tier_12 <- read.csv("Data/demographics/epic_dems.csv") %>%
+  clean_names() %>%
+  filter(tier != "3") %>%
+  distinct(pwsid)
+
+hq_states <-c("CA", "NJ", "NM", "CT", "WA")
+
+
+################################################################################
+## Select tier 1 and tier 2 state boundaries for each type of boundary to compare
+################################################################################
+
+## Create a loop to get rel risk for tier 1 & 2 states
+
+# Set up the function call 
+
+all_risk <- data.frame()
+
+pws_obs <- data.frame(matrix(ncol = 3, nrow = 0))
+
+colnames(pws_obs) <- c("obs", "indicator", "boundary")
+
+for (b in boundary) {
+  
+  hb_vio <- readRDS(paste0('data/combined/pwsid/hb_vio_',b ,'.rds'))
+  
+  lcr_vio <- readRDS(paste0('data/combined/pwsid/lcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pb_vio_count)
+  
+  pfas_vio <- readRDS(paste0('data/combined/pwsid/pfas_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pfas_count)
+  
+  dbp_vio <- readRDS(paste0('data/combined/pwsid/dbp_vio_',b ,'.rds')) %>%
+    mutate(total_violations = combined_dbp)
+  
+  tcr_vio <- readRDS(paste0('data/combined/pwsid/tcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = detection_share)
+  
+  data_list <- list(hb_vio, lcr_vio, pfas_vio, dbp_vio, tcr_vio)
+  
+  my_df <- data.frame(risk_type = c("race", "income"))
+  
+  for (j in 1:length(data_list)) {
+    
+    water_data <- data_list[[j]]
+    
+    i = nth(indicator, j)
+    
+    rel_risk <- water_data %>%
+      filter(pwsid %in% tier_12$pwsid) %>%
+      filter(pop_served != 0)%>% #remove systems with no pop served
+      mutate(whitepct = (1-minorpct)) %>%
+      mutate(minor_served = minorpct*pop_served) %>%
+      mutate(nhw_served = whitepct*pop_served) %>% #non hispanic white served 
+      mutate(minor_risk = weighted.mean(total_violations, minor_served, na.rm= TRUE)) %>% #black population weight
+      mutate(nhw_risk = weighted.mean(total_violations, nhw_served, na.rm= TRUE)) %>%   #non-hispanic white
+      mutate(highinc = (1-lowinc)) %>% #create high income category
+      mutate(highinc_served = highinc*pop_served) %>% #Find total population served in each category
+      mutate(lowinc_served = lowinc*pop_served) %>%
+      mutate(highinc_risk = weighted.mean(total_violations, highinc_served, na.rm= TRUE)) %>% #High income weight
+      mutate(lowinc_risk = weighted.mean(total_violations, lowinc_served, na.rm= TRUE)) #low income weight
+    
+    rel_risk_r = as.numeric(rel_risk[1, "minor_risk"]) / as.numeric(rel_risk[1, "nhw_risk"])
+    rel_risk_i = as.numeric(rel_risk[1, "lowinc_risk"]) / as.numeric(rel_risk[1, "highinc_risk"])
+    
+    risks <- data.frame(risk = c(rel_risk_r, rel_risk_i))
+    
+    my_df <- cbind(my_df, risks)
+    
+    colnames(my_df)[colnames(my_df) == "risk"] <- paste0(i, "_rel_risk")
+    
+    rm(risks)
+    
+    obs <- sum(!is.na(rel_risk$total_violations))
+    
+    pws_obs[nrow(pws_obs) + 1, ] = c(obs, i, b)
+    
+  }
+  
+  my_df <- my_df %>% 
+    mutate(boundary = as.factor(b))
+  
+  all_risk <- bind_rows(all_risk, my_df)  
+  
+}
+
+write.csv(all_risk, file = "Data/rel_risk/rel_risk_tier12_all.csv")
+
+all_risk <- read.csv("Data/rel_risk/rel_risk_tier12_all.csv") 
+
+
+################################################################################
+## Select tier 1 and tier 2 state boundaries for each type of boundary to compare
+################################################################################
+
+## Create a loop to get rel risk for HQ states
+
+# Set up the function call 
+
+all_risk <- data.frame()
+
+pws_obs <- data.frame(matrix(ncol = 3, nrow = 0))
+
+colnames(pws_obs)<- c("obs", "indicator", "boundary")
+
+for (b in boundary) {
+  
+  hb_vio <- readRDS(paste0('data/combined/pwsid/hb_vio_',b ,'.rds'))
+  
+  lcr_vio <- readRDS(paste0('data/combined/pwsid/lcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pb_vio_count)
+  
+  pfas_vio <- readRDS(paste0('data/combined/pwsid/pfas_vio_',b ,'.rds')) %>%
+    mutate(total_violations = pfas_count)
+  
+  dbp_vio <- readRDS(paste0('data/combined/pwsid/dbp_vio_',b ,'.rds')) %>%
+    mutate(total_violations = combined_dbp)
+  
+  tcr_vio <- readRDS(paste0('data/combined/pwsid/tcr_vio_',b ,'.rds')) %>%
+    mutate(total_violations = detection_share)
+  
+  data_list <- list(hb_vio, lcr_vio, pfas_vio, dbp_vio, tcr_vio)
+  
+  my_df <- data.frame(risk_type = c("race", "income"))
+  
+  for (j in 1:length(data_list)) {
+    
+    water_data <- data_list[[j]]
+    
+    i = nth(indicator, j)
+    
+    rel_risk <- water_data %>%
+      filter(st_abb %in% hq_states) %>%
+      filter(pop_served != 0)%>% #remove systems with no pop served
+      mutate(whitepct = (1-minorpct)) %>%
+      mutate(minor_served = minorpct*pop_served) %>%
+      mutate(nhw_served = whitepct*pop_served) %>% #non hispanic white served 
+      mutate(minor_risk = weighted.mean(total_violations, minor_served, na.rm= TRUE)) %>% #black population weight
+      mutate(nhw_risk = weighted.mean(total_violations, nhw_served, na.rm= TRUE)) %>%   #non-hispanic white
+      mutate(highinc = (1-lowinc)) %>% #create high income category
+      mutate(highinc_served = highinc*pop_served) %>% #Find total population served in each category
+      mutate(lowinc_served = lowinc*pop_served) %>%
+      mutate(highinc_risk = weighted.mean(total_violations, highinc_served, na.rm= TRUE)) %>% #High income weight
+      mutate(lowinc_risk = weighted.mean(total_violations, lowinc_served, na.rm= TRUE)) #low income weight
+    
+    rel_risk_r = as.numeric(rel_risk[1, "minor_risk"]) / as.numeric(rel_risk[1, "nhw_risk"])
+    rel_risk_i = as.numeric(rel_risk[1, "lowinc_risk"]) / as.numeric(rel_risk[1, "highinc_risk"])
+    
+    risks <- data.frame(risk = c(rel_risk_r, rel_risk_i))
+    
+    my_df <- cbind(my_df, risks)
+    
+    colnames(my_df)[colnames(my_df) == "risk"] <- paste0(i, "_rel_risk")
+    
+    rm(risks)
+    
+    obs <- sum(!is.na(rel_risk$total_violations))
+    
+    pws_obs[nrow(pws_obs) + 1, ] = c(obs, i, b)
+    
+  }
+  
+  my_df <- my_df %>% 
+    mutate(boundary = as.factor(b))
+  
+  all_risk <- bind_rows(all_risk, my_df)  
+
+}
+
+write.csv(all_risk, file = "Data/rel_risk/rel_risk_HQ_all.csv")
+
+all_risk <- all_risk %>%
+  group_by(race_cat)
+
+all_risk <- read.csv("Data/rel_risk/rel_risk_HQ_all.csv") 
+
+################################################################################
+
+
