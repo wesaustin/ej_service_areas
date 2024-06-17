5b2d3136f9ad164be1dd6a9c8ed59b7b4787a180
tina-bardot <142334011+tina-bardot@users.noreply.github.com>
2024-06-17 12:17:53 -0400

    Update 02d_interpolate_areal.R
    Adding minor edits

--- code/02d_interpolate_areal.R
+++ code/02d_interpolate_areal.R
@@ -1,48 +1,25 @@
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  readr,
+  tidyverse,
+  sf, # vector data operations
+  raster, # raster data operations
+  exactextractr, # fast raster data extraction for polygons
+  maps, # to get county boundary data
+  data.table, # data wrangling
+  dplyr, # data wrangling
+  lubridate, # Date object handling
+  tidycensus,
+  mapview,
+  janitor,
+  EJSCREENbatch, # Download EJscreen 
+  areal #for areal interpolation
+)
+
 # hm interpolation
 
 save_here <- "Data/hm_interp/"
-
-sb_hm_shp <- sf::st_read("Data/hall_murray/Water_System_Boundaries.gpkg") 
-
-data.tog <- readRDS("Data/data.tog.rds") %>%
-  st_as_sf() %>%
-  st_transform(crs = st_crs(sb_hm_shp))
-
-st_crs(data.tog)
-st_crs(sb_hm_shp)
-
-data.nog <- data.tog %>%
-  st_drop_geometry() %>%
-  mutate(ID = as.character(ID), 
-         ID = str_pad(ID, 12, "left", pad = "0"))
-
-ext <- names(data.nog[, c(4)])
-
-int <- names(data.nog[, c(5:56)])
-
-# Need to make data planar for areal interpolation
-sb_hm_sim  <- sb_hm_shp %>% 
-  st_make_valid() %>%
-  st_simplify %>% 
-  st_transform(crs = 3857)
-
-data.pl <- data.tog %>%
-  st_transform(crs = 3857)
-
-# data.pl <- spTransform(data.tog, CRS("+proj=utm +zone=10 +datum=WGS84"))
-
-#Validation protocol
-areal::ar_validate(target = sb_hm_sim, source= data.pl, varList = int, verbose = TRUE)
-
-hm_interp <- areal::aw_interpolate(sb_hm_sim,
-                                   tid = "PWSID",
-                                   sid = 'ID',
-                                   source = data.pl,
-                                   weight = "sum",
-                                   output = 'sf',
-                                   extensive = "ACSTOTPOP",
-                                   intensive = int)
-
 
 target <- sf::st_read("Data/hall_murray/Water_System_Boundaries.gpkg") 
 
@@ -63,23 +40,29 @@ ext <- "ACSTOTPOP"
 
 int <- names(data.nog[, c(5:56)])
 
+rm(data.nog)
+
 
 interpolate <- function (source, sid, target, tid, ext, int) {
   
   # Find intersecting polygons
 
   intersect <- st_intersection(target, source)
-
+  
   #Create df with area of intersection
   intersect_area <- intersect %>% 
     mutate(intersect_area = st_area(.)) %>%   # create new column with shape area
     st_drop_geometry() %>% #drop geometry: easier to compute
     dplyr::select(c(tid, sid, 'intersect_area')) # only select columns needed to merge
   
+  intersect_area <- drop_units(intersect_area) 
+  
   #Calculate the area of the source polygons (for extensive interpolation)
   source_area <- mutate(source, sid_area = st_area(source))  %>%
     dplyr::select(sid, 'sid_area') %>% # Calculate the area of the source polygon
     st_drop_geometry()
+  
+  source_area <- drop_units(source_area)
   
   #merge by sid, keep tid
   source_area <- left_join(source_area, intersect_area, by = sid, relationship="many-to-many") %>% #Merge by id no, relationship many
@@ -90,16 +73,16 @@ interpolate <- function (source, sid, target, tid, ext, int) {
     dplyr::select(tid, 'tid_area') %>% # Calculate the area of the target polygon
     st_drop_geometry()
   
+  target_area <- drop_units(target_area)
+  
   # Merge by tid, keep sid 
   target_area <- left_join(target_area, intersect_area, by = tid, relationship="many-to-many") %>% #Merge by id no, relationship many
     mutate(target_coverage = as.numeric(intersect_area/tid_area)) # Calculate % area of each source polygon to the overall target area
   
   intersect <- left_join(intersect, target_area, by = c(tid, sid), relationship="many-to-many") %>%
     left_join(source_area, by = c(sid, tid), relationship="many-to-many") %>%
-    group_by(c(sid)) %>%
+    rowwise %>%
     mutate(across(all_of(int), ~ .x*target_coverage))  %>%
-    ungroup() %>%
-    group_by(c(tid)) %>%
     mutate(across(all_of(ext), ~ .x*source_coverage))  
   
   target_dat <- intersect %>%
@@ -125,10 +108,14 @@ interpolate <- function (source, sid, target, tid, ext, int) {
   
 }
 
+saveRDS(intersect, file = paste0(save_here, "intersect.rds"))
+
 saveRDS(target_dat, file = paste0(save_here,"target_dat.rds"))
 
 saveRDS(source_dat, file = paste0(save_here,"source_dat.rds"))
 
+
 interpolate(source=data.tog, sid = "ID", target=sb_hm_shp, tid="PWSID", ext="ACSTOTPOP", int)
+
 
 rm(taget_area, source_area, intersect_area)
