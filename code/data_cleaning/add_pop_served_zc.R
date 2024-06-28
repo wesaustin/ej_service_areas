e5b11900334dcf6b9ffda1bff6a68eb825a2dfdb
wesaustin <austin.wes@epa.gov>
2024-06-28 14:36:11 -0400

    Update add_pop_served_zc.R

--- code/data_cleaning/add_pop_served_zc.R
+++ code/data_cleaning/add_pop_served_zc.R
@@ -1,16 +1,27 @@
-# Get pop_served in zc boundaries
+# Data Cleaning to Ensure all PWS Demographic Files use Consistent population served 
 
-zc_dem <- read.csv(paste0("Data/demographics/zc_dems.csv"))
+
+
+
+
+##############################################
+# Zipcodes
+##############################################
+
+
+zc_dem <- read.csv(paste0("data/demographics/zc_dems.csv"))
 
 zc_dem_comb <- read.csv(paste0("Data/demographics/epic_dems.csv")) %>%
-  select(pwsid, population_served_count) %>%
+  dplyr::select(pwsid, population_served_count) %>%
   right_join(zc_dem, by = 'pwsid') %>%
     clean_names() %>%
-  write.csv("Data/demographics/zc_dems.csv")
+  write.csv("data/demographics/zc_dems.csv")
 
+##############################################
 # Counties
+##############################################
 
-county_dem <- read.csv(paste0("Data/demographics/county_dems.csv")) 
+county_dem <- read.csv(paste0("data/demographics/county_dems.csv")) 
 
 duplicated_columns <- duplicated(t(county_dem))
 
@@ -19,21 +30,24 @@ colnames(county_dem[duplicated_columns])
 
 # Remove the Duplicated Columns
 county_dem <- county_dem[!duplicated_columns]
-
 county_dem <- rename_with(county_dem, ~ tolower(gsub(".x", "", .x, fixed = TRUE)))
 
-
-county_dem_comb <- read.csv(paste0("Data/demographics/epic_dems.csv")) %>%
-  select(pwsid, population_served_count) %>%
+county_dem_comb <- read.csv(paste0("data/demographics/epic_dems.csv")) %>%
+  dplyr::select(pwsid, population_served_count) %>%
   right_join(county_dem, by = 'pwsid') %>%
   clean_names() %>%
-  write.csv("Data/demographics/county_dems.csv")
+  write.csv("data/demographics/county_dems.csv")
 
 
-hm_dem <- read.csv(paste0("Data/demographics/hm_dems_final.csv"))
+##############################################
+# H&M aka EPA ORD Boundaries
+##############################################
 
-hm_dem_comb <- read.csv(paste0("Data/demographics/epic_dems.csv")) %>%
+hm_dem <- read.csv(paste0("data/demographics/hm_dems.csv")) %>%
+  clean_names() 
+
+hm_dem_comb <- read.csv(paste0("data/demographics/epic_dems.csv")) %>%
   dplyr::select(pwsid, population_served_count) %>%
   right_join(hm_dem, by = 'pwsid') %>%
   clean_names() %>%
-  write.csv("Data/demographics/hm_dems_final.csv")
+  write.csv("data/demographics/hm_dems.csv")
